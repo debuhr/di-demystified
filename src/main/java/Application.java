5dc1fefb3a07fe995409227266d5ab440c31ce90
Jonas de Buhr <jonas.de.buhr@gmx.net>
2025-02-22 14:00:00 +0100

    Add TODOs for dependency injection

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,6 +10,8 @@ public class Application {
     public static final Executor DEFAULT_EXECUTOR = null;
 
     public static void main(String[] args) throws IOException {
+        // TODO (jdb): add dependency injection and refactor Application and Router to use it
+        // --
         // TODO (jdb): refactor to finding classes extending HttpHandler on startup and querying the path from them
         // TODO (jdb): refactor to use annotations to define handlers
         // TODO (jdb): mark GET and POST handlers, maybe with annotations and reject wrong requests
@@ -19,6 +21,8 @@ public class Application {
     }
 
     public static void run() throws IOException {
+        // TODO (jdb): the run method has the responsibility of creating the instances of the classes that make up the
+        //  running application. This is not good, we should separate this concept and call it from main.
         HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
         ResponseSender responseSender = new ResponseSender();
         Router router = new Router(server, responseSender);
